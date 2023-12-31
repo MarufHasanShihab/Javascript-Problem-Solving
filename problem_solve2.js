732fe6c7bd12916e657ce794e4e6a49866fa09d5
Maruf Hasan Shihab <marufhasanshihab2@gmail.com>
2023-12-31 10:37:50 +0600

    added the new problem

--- problem_solve2.js
+++ problem_solve2.js
@@ -3,6 +3,25 @@
 const sentence = "Lwarn with sumit is all about teaching web development skill and techniques in an efficient an practical manner. If you are just getting started in web development, learn with sumit has all the tools you need to learn the newest and most popular technologies to convert you from no stack to full stack developer. Learn with sumit alson deep dives into advence topics using the latest best pratices foy you seasoned web developers. "
 
 const matches = sentence.match(/sumit/gi);
-console.log(matches)
-const ocuranes = matches.length
-console.log(ocuranes)
+const occuranes = matches ? matches.length : 0;
+console.log(occuranes)
+
+let postion = sentence.search(/sumit/i);
+postion = postion >= 0 ? postion : 'not found!';
+console.log(postion)
+
+
+//problem 2
+//input: linearSearch(['a,' 'b', 'c', 'd', 'c'] 'c');
+// output: 2 or 'not found!'
+// problem leaneseach function টি implement করে দেখান
+function learnerSearch(array, value){
+    for(var i = 0; i < array.length; i++){
+        if(array[i] === value){
+            return i;
+        }
+    }
+    return 'Not Found!'
+}
+
+console.log(learnerSearch(['a', 'b', 'c', 'd', 'c'], 'c'));
